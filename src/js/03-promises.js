b743be133fdd269ad15a0922485782c38a772d99
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-14 23:54:33 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inputForm = document.querySelector('.form');
+
+inputForm.addEventListener('submit', onSubmitClick);
+
+function onSubmitClick(event) {
+  event.preventDefault();
+  let delay = Number(inputForm.elements.delay.value);
+  const step = Number(inputForm.elements.step.value);
+  const amount = Number(inputForm.elements.amount.value);
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
